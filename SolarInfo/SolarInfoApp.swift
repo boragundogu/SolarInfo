de052643400a79478d826c0b1c7de4b7a9d4de6d
boragundogu <boratelif1@gmail.com>
2023-05-07 15:31:57 +0300

    Initial Commit

---
+++ SolarInfo/SolarInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarInfoApp.swift
+//  SolarInfo
+//
+//  Created by Bora Gündoğu on 7.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SolarInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
